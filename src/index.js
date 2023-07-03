21783cf4ae847efdaaaf5096fcb68d02c868858c
Ravi Prakash <120682913+raviprakash1998@users.noreply.github.com>
2023-07-04 02:56:02 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
   // write code here
   // dont change function name
+var createStream = fs.createWriteStream(filename);
+writeStream.write(fileContent);
+createStream.end();
 };
 
 module.exports = { writeFile };
